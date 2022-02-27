23c7617f207cbd85d9fc745bf12d0da6d8959dc3
MHMK <khanihasan1381@gmail.com>
2022-02-27 19:53:32 +0330

    Initial Setting

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,12 +1,32 @@
 package sample;
 
+
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+
+
 
 public class Controller
 {
+    private double result;
+    private char operator;
+    private String first;
+    private String second;
+
+    public Controller()
+    {
+        result = 0;
+        operator = ' ';
+        first = "";
+        second = "";
+    }
+
+
+
     @FXML
     private Button zeroButton;
     @FXML
@@ -47,5 +67,186 @@ public class Controller
     private Button clearButton;
     @FXML
     private Button xButton;
+    @FXML
+    private TextField resultTxt;
 
+    public void clickButton(ActionEvent event)
+    {
+        if(event.getSource() == zeroButton)
+        {
+            first += "0";
+            resultTxt.setText(first);
+        }
+        else if(event.getSource() == oneButton)
+        {
+            first += "1";
+            resultTxt.setText(first);
+        }
+        else if(event.getSource() == twoButton)
+        {
+            first += "2";
+            resultTxt.setText(first);
+        }
+        else if(event.getSource() == threeButton)
+        {
+            first += "3";
+            resultTxt.setText(first);
+        }
+        else if(event.getSource() == fourButton)
+        {
+            first += "4";
+            resultTxt.setText(first);
+        }
+        else if(event.getSource() == fiveButton)
+        {
+            first += "5";
+            resultTxt.setText(first);
+        }
+        else if(event.getSource() == sixButton)
+        {
+            first += "6";
+            resultTxt.setText(first);
+        }
+        else if(event.getSource() == sevenButton)
+        {
+            first += "0";
+            resultTxt.setText(first);
+        }
+        else if(event.getSource() == eightButton)
+        {
+            first += "8";
+            resultTxt.setText(first);
+        }
+        else if(event.getSource() == nineButton)
+        {
+            first += "9";
+            resultTxt.setText(first);
+        }
+        else if(event.getSource() == dotButton)
+        {
+            if(!first.contains("."))
+            {
+                if(first.equals(""))
+                {
+                    first += "0.";
+                    resultTxt.setText(first);
+                }
+                else
+                {
+                    first += ".";
+                    resultTxt.setText(first);
+                }
+            }
+        }
+        else if(event.getSource() == plusButton)
+        {
+            if(!first.equals(""))
+            {
+                second = first;
+                first = "";
+                operator = '+';
+                resultTxt.setText(first + " + ");
+            }
+            else if(!second.equals(""))
+            {
+                operator = '-';
+            }
+        }
+        else if(event.getSource() == minesButton)
+        {
+            if(!first.equals(""))
+            {
+                second = first;
+                first = "";
+                operator = '-';
+                resultTxt.setText(first + " - ");
+            }
+            else if(!second.equals(""))
+            {
+                operator = '-';
+            }
+        }
+        else if(event.getSource() == multiButton)
+        {
+            if(!first.equals(""))
+            {
+                second = first;
+                first = "";
+                operator = '*';
+                resultTxt.setText(first + " x ");
+            }
+            else if(!second.equals(""))
+            {
+                operator = '-';
+            }
+        }
+        else if(event.getSource() == divideButton)
+        {
+            if(!first.equals(""))
+            {
+                second = first;
+                first = "";
+                operator = '/';
+                resultTxt.setText(first + " ÷ ");
+            }
+            else if(!second.equals(""))
+            {
+                operator = '-';
+            }
+        }
+        else if(event.getSource() == PiButton)
+        {
+            first = "3.1415926";
+        }
+        else if(event.getSource() == EButton)
+        {
+            first = "2.7182818";
+        }
+        else if(event.getSource() == equalButton)
+        {
+            if(!first.equals("") && !second.equals(""))
+            {
+                if(operator == '+')
+                {
+                    result = Double.parseDouble(second) + Double.parseDouble(first);
+                }
+                else if(operator == '-')
+                {
+                    result = Double.parseDouble(second) - Double.parseDouble(first);
+                }
+                else if(operator == '*')
+                {
+                    result = Double.parseDouble(second) * Double.parseDouble(first);
+                }
+                else
+                {
+                    try
+                    {
+                        result = Double.parseDouble(second) + Double.parseDouble(first);
+                    }
+                    catch(Exception e)
+                    {
+                        result = 0;
+                        System.out.println("Divide by zero");
+                    }
+                }
+            }
+        }
+        else if(event.getSource() == clearButton)
+        {
+            first = "";
+            second = "";
+            operator = ' ';
+            result = 0;
+        }
+        else if(event.getSource() == xButton)
+        {
+            if(!first.equals(""))
+            {
+                double curr = -Double.parseDouble(first);
+
+                first = Double.toString(curr);
+            }
+        }
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,20 +5,42 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
+import javax.swing.text.html.ImageView;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        stage.setTitle("Hello World");
+        stage.setTitle("Calculator");
         stage.setResizable(false);
-        Scene scene = new Scene(root);
 
+        stage.setOnCloseRequest(e ->
+        {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation Dialog");
+            alert.setHeaderText("Are you sure you want to exit?");
+            alert.setContentText("Are you ok with this?");
+            if(alert.showAndWait().get() == ButtonType.OK)
+            {
+                stage.close();
+            }
+        });
+
+
+        Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("Main.css").toExternalForm());
+        Controller controller = new Controller();
+
+        Image image = new Image(getClass().getResourceAsStream("icon.png"));
+        stage.getIcons().add(image);
 
         stage.setScene(scene);
         stage.show();
